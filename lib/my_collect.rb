04b2aa9c065c4a7208f4344e8298aaca9b7ef8d4
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2019-04-13 22:33:46 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,10 +1,15 @@
 def my_collect(collection)
   i = 0
-  array = []
+  list = []
 
   while i < collection.length
-    array < yield(collection[i])
+    list < yield(collection[i])
     i += 1
   end
-  array
+  list
+end
+
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+my_collect(array) do |name|
+  name.split(" ").first
 end
